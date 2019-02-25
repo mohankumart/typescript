5dae4c0f4802291b1d18cbb6bbaea7cb725ddf15
Mohan Kumar <tmk_mohankumar@yahoo.com>
2019-02-25 23:59:51 +0530

    new changes

---
+++ modules/app.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var circle_js_1 = require("./math/circle.js");
+console.log("Hello");
+console.log(circle_js_1.calculateArea(3));
---
+++ modules/math/circle.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.PI = 3.14;
+function calculateArea(radius) {
+    return exports.PI * radius * radius;
+}
+exports.calculateArea = calculateArea;
