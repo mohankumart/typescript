c12569d20076e6d04e2b27fd4003bfeaa4857599
Mohan Kumar <tmk_mohankumar@yahoo.com>
2019-02-25 12:45:21 +0530

    inheritence changes

--- classes/class.ts
+++ classes/class.ts
@@ -1,17 +1,35 @@
 class Person {
 	public name: string;
-	private type: string;
-	protected age: number;
+	private type: string = 'stduent';
+	protected age: number = 28;
 	
 	constructor(name: string, public username: string){
 		this.name = name;
-		this.type = 'student';
-		this.age = 28;
+	}	
+	
+	printAge(){
+		console.log(this.age);
+		this.setType("Student");
 	}	
 
+	private setType(type: string){
+		this.type = type;
+		console.log(this.type);
+	}
 }
-debugger;
+
 const person = new Person('Mohan', 'mohan');
+person.printAge();
+// person.setType("Student"); // wont work as setType is private method
 
+// Inheritance
+class Max extends Person {
+	constructor(username: string){
+		super("Max", username);
+		this.age = 31; // can access protected memebers from inherited class
+		// console.log(this.type); Cant access the private members from inherited class
+	}
+}
 
-
+const max = new Max("max");
+console.log(max);
