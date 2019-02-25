98e5a8c178ce3d678055f4ba1291a28cc6b14b0d
Mohan Kumar <tmk_mohankumar@yahoo.com>
2019-02-25 14:00:43 +0530

    complied

---
+++ classes/staticMethods.js
@@ -0,0 +1,14 @@
+"use strict";
+// static properties and methods
+var Helpers = /** @class */ (function () {
+    function Helpers() {
+    }
+    Helpers.calcCircumference = function (diameter) {
+        return this.PI * diameter;
+    };
+    Helpers.PI = 3.14;
+    return Helpers;
+}());
+console.log(2 * Helpers.PI);
+console.log(Helpers.calcCircumference(8));
+//# sourceMappingURL=staticMethods.js.map
